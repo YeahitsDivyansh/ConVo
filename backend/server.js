9367d4f107f4faf845c9c2fa2f4fb6c2b600f085
YeahitsDivyansh <divyansh972003@gmail.com>
2024-10-17 15:26:49 +0530

    port modification

--- backend/server.js
+++ backend/server.js
@@ -44,9 +44,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-const server = app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
 
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
